1d9a37ee6f6136df2fd02ad38fe5eab35111d0fe
WaelS4 <wael.sellami.44@gmail.com>
2021-07-13 14:47:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddMovie from "./components/AddMovie/AddMovie";
+import MovieList from "./components/MovieList/MovieList";
+import NavBar from "./components/NavBar/NavBar";
+import { moviedata } from "./data";
 function App() {
+  const [movielist, setMovielist] = useState(moviedata);
+  const [title, setTitle] = useState("");
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const [rate, setRate] = useState(0);
+  const ratingChanged = (newRating) => {
+    setRate(newRating);
+  };
+  const handleAdd = (newMovie) => {
+    setMovielist([...movielist, newMovie]);
+  };
+  const handleDelete = (id) => {
+    setMovielist(movielist.filter((movie) => movie.id !== id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar
+        title={title}
+        handleChange={handleChange}
+        rate={rate}
+        ratingChanged={ratingChanged}
+      />
+      <MovieList
+        handleDelete={handleDelete}
+        movielist={movielist.filter(
+          (movie) =>
+            movie.title.toUpperCase().includes(title.toUpperCase()) &&
+            movie.rate >= rate
+        )}
+      />
+      <AddMovie handleAdd={handleAdd} />
     </div>
   );
 }
